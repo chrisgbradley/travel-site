0ba3b7c7bb70e79c3822b2013223c89f41944553
Christian Bradley <chris@chrisgbradley.dev>
2019-03-23 00:03:52 -0400

    add scroll reveal to feature items using waypoints

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,30 @@
+import $ from "jquery";
+import waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoints";
+
+class RevealOnScroll {
+
+	constructor() {
+		this.itemsToReveal = $(".feature-item");
+		this.hideInitially();
+		this.createWaypoints();
+	}
+
+	hideInitially() {
+		this.itemsToReveal.addClass("reveal-item")
+	}
+
+	createWaypoints() {
+		this.itemsToReveal.each(function() {
+			var item = this;
+			new Waypoint({
+				element: item,
+				handler: function(direction) {
+					$(item).addClass("reveal-item--is-visible");
+				},
+				offset: "85%"
+			})
+		})
+	}
+}
+
+export default RevealOnScroll;
